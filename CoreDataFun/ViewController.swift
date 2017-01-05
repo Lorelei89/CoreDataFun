8dae8e04bde09694046403decdbd33166f341315
Lorelei89 <sabina_buruiana@yahoo.com>
2017-01-05 17:18:10 +0200

    Initial Commit

---
+++ CoreDataFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataFun
+//
+//  Created by sabina on 05/01/17.
+//  Copyright © 2017 sabina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoreDataFunTests/CoreDataFunTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataFunTests.swift
+//  CoreDataFunTests
+//
+//  Created by sabina on 05/01/17.
+//  Copyright © 2017 sabina. All rights reserved.
+//
+
+import XCTest
+@testable import CoreDataFun
+
+class CoreDataFunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
