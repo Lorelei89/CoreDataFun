8eb65fbc9e8f11333cb9ec09b6e35eaffe723bbc
Lorelei89 <sabina_buruiana@yahoo.com>
2017-01-05 18:27:08 +0200

    NSFetchRequest add inside an array the users with name and password

--- CoreDataFun/ViewController.swift
+++ CoreDataFun/ViewController.swift
@@ -11,23 +11,56 @@ import CoreData
 
 class ViewController: UIViewController {
 
+    
+    var passWordArray:[String] = []
+    var usernameArray:[String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Storing core data
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
         //allows as to work with core data
         let context = appDelegate.persistentContainer.viewContext
-        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
-        newUser.setValue("Sabina", forKey:"username")
-        newUser.setValue("Sabina123", forKey:"password")
+//        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+//        newUser.setValue("Sorina", forKey:"username")
+//        newUser.setValue("Sorina123", forKey:"password")
+//
+//        do {
+//            try context.save() 
+//             print("SAVED")
+//        } catch {
+//            // PROCESS ERROR
+//        }
+//        
+        //if you don't want to repeat for 4 time the same stepts with different users we do like that
         
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName:"Users")
+        request.returnsObjectsAsFaults = false
         
         do {
-            try context.save() 
-             print("SAVED")
+            //put data into core data
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]{
+                    if let username = result.value(forKey: "username")  as? String {
+                        //USERNAME
+                       // print(username)
+                        usernameArray.append(username)
+                    }
+                    if let password = result.value(forKey: "password")  as? String {
+                        //PASSWORD
+                      //  print(password)
+                        passWordArray.append(password)
+                    }
+                }
+            }
+            print(passWordArray)
+            print(usernameArray)
+            //print("SAVED")
         } catch {
             // PROCESS ERROR
         }
+        
     }
 
     override func didReceiveMemoryWarning() {
